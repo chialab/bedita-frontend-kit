509766261d5e6ddd31a59ee5b054e3308044c264
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2021-04-10 02:26:14 +0200

    refactor: re-sort methods, add deprecation warnings

--- src/View/Helper/PosterHelper.php
+++ src/View/Helper/PosterHelper.php
@@ -74,20 +74,6 @@ class PosterHelper extends Helper
     }
 
     /**
-     * Check if object has a valid poster, or is an Image itself, and the referenced file does actually exist.
-     *
-     * @param \BEdita\Core\Model\Entity\ObjectEntity|null $object Object entity.
-     * @param bool $forceSelf Restrict checks to the object itself, rather than evaluating its `poster` related objects.
-     * @param int $variant Poster variant, i.e. priority of the desired poster related object.
-     * @return bool
-     * @deprecated Use {@see \Chialab\FrontendKit\View\Helper\PosterHelper::exists()} instead.
-     */
-    public function check(?ObjectEntity $object, bool $forceSelf = false, int $variant = 0): bool
-    {
-        return $this->exists($object, compact('forceSelf', 'variant'));
-    }
-
-    /**
      * Yield candidates to be the poster.
      *
      * @param \BEdita\Core\Model\Entity\ObjectEntity|null $object
@@ -122,6 +108,22 @@ class PosterHelper extends Helper
     /**
      * Check if object has a valid poster, or is an Image itself, and the referenced file does actually exist.
      *
+     * @param \BEdita\Core\Model\Entity\ObjectEntity|null $object Object entity.
+     * @param bool $forceSelf Restrict checks to the object itself, rather than evaluating its `poster` related objects.
+     * @param int $variant Poster variant, i.e. priority of the desired poster related object.
+     * @return bool
+     * @deprecated Use {@see \Chialab\FrontendKit\View\Helper\PosterHelper::exists()} instead.
+     */
+    public function check(?ObjectEntity $object, bool $forceSelf = false, int $variant = 0): bool
+    {
+        deprecationWarning('PosterHelper::check() is deprecated, use PosterHelper::exists() instead.');
+
+        return $this->exists($object, compact('forceSelf', 'variant'));
+    }
+
+    /**
+     * Check if object has a valid poster, or is an Image itself, and the referenced file does actually exist.
+     *
      * ### Poster options:
      *
      * - `forceSelf`: restrict checks to the object itself, rather than evaluating its `poster` related objects. Default: `false`
@@ -144,20 +146,6 @@ class PosterHelper extends Helper
     }
 
     /**
-     * Get URL for poster image.
-     *
-     * @param \BEdita\Core\Model\Entity\ObjectEntity|null $object Object entity.
-     * @param bool $forceSelf Restrict checks to the object itself, rather than evaluating its `poster` related objects.
-     * @param int $variant Poster variant, i.e. priority of the desired poster related object.
-     * @return string|null
-     * @deprecated Use {@see \Chialab\FrontendKit\View\Helper\PosterHelper::url()} instead.
-     */
-    public function getUrl(?ObjectEntity $object, bool $forceSelf = false, int $variant = 0): ?string
-    {
-        return $this->url($object, false, compact('forceSelf', 'variant'));
-    }
-
-    /**
      * Get media URL, optionally generating a thumbnail.
      *
      * @param \BEdita\Core\Model\Entity\Media $media Media entity.
@@ -175,6 +163,22 @@ class PosterHelper extends Helper
         }
 
         return null;
+    }
+
+    /**
+     * Get URL for poster image.
+     *
+     * @param \BEdita\Core\Model\Entity\ObjectEntity|null $object Object entity.
+     * @param bool $forceSelf Restrict checks to the object itself, rather than evaluating its `poster` related objects.
+     * @param int $variant Poster variant, i.e. priority of the desired poster related object.
+     * @return string|null
+     * @deprecated Use {@see \Chialab\FrontendKit\View\Helper\PosterHelper::url()} instead.
+     */
+    public function getUrl(?ObjectEntity $object, bool $forceSelf = false, int $variant = 0): ?string
+    {
+        deprecationWarning('PosterHelper::getUrl() is deprecated, use PosterHelper::url() instead.');
+
+        return $this->url($object, false, compact('forceSelf', 'variant'));
     }
 
     /**
