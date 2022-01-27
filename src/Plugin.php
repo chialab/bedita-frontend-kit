3fe2c3772b69897a4fc59c44c2e01f3d2e9ffeb5
Andres Bautista <andres.bautista@chialab.it>
2022-01-27 16:28:19 +0100

    feat: add PagesController.php and add method routes in Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,10 +3,36 @@
 namespace Chialab\FrontendKit;
 
 use Cake\Core\BasePlugin;
+use Cake\Routing\RouteBuilder;
 
 /**
  * Plugin for Chialab\FrontendKit
  */
 class Plugin extends BasePlugin
 {
+    /**
+     * @inheritDoc
+     */
+    public function routes($routes): void
+    {
+        $routes->plugin(
+            'Chialab/FrontendKit',
+            ['path' => '/'],
+            function ($routes) {
+                $routes->connect('/', ['controller' => 'Pages', 'action' => 'home'], ['_name' => 'pages:home']);
+
+                $routes->connect(
+                    '/objects/{uname}',
+                    ['controller' => 'Pages', 'action' => 'object'],
+                    ['_name' => 'pages:objects', 'pass' => ['uname'], 'routeClass' => ObjectRoute::class]
+                );
+
+                $routes->connect(
+                    '/**',
+                    ['controller' => 'Pages', 'action' => 'fallback'],
+                    ['_name' => 'pages:fallback']
+                );
+            }
+        );
+    }
 }
