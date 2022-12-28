3e3be4787b953408149aa0cc6da1a41ee99aac29
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-12-28 15:41:39 +0100

    fix: allow null return

--- src/View/Helper/ManagerHelper.php
+++ src/View/Helper/ManagerHelper.php
@@ -24,7 +24,7 @@ class ManagerHelper extends Helper
      *
      * @return \ArrayAccess|array
      */
-    protected function getObject(): ArrayAccess|array
+    protected function getObject(): ArrayAccess|array|null
     {
         $candidates = array_filter([
             $this->getView()->get('_main'),
