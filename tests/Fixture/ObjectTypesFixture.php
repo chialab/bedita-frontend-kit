811f6d8fc1e8bccc2ba9dbe3235c7b40af818bca
Nicolò Carpignoli <nicolocarpignoli@gmail.com>
2024-03-28 17:03:02 +0100

    test: remove `images` from object types fixture as it is now included in bedita/core

--- tests/Fixture/ObjectTypesFixture.php
+++ tests/Fixture/ObjectTypesFixture.php
@@ -8,28 +8,5 @@ use BEdita\Core\Test\Fixture\ObjectTypesFixture as BEObjectTypesFixture;
  */
 class ObjectTypesFixture extends BEObjectTypesFixture
 {
-    public function init(): void
-    {
-        // 11
-        $this->records[] = [
-            'singular' => 'image',
-            'name' => 'images',
-            'is_abstract' => false,
-            'parent_id' => 8,
-            'tree_left' => 17,
-            'tree_right' => 18,
-            'description' => null,
-            'plugin' => 'BEdita/Core',
-            'model' => 'Media',
-            'associations' => ['Streams'],
-            'created' => '2017-11-10 09:27:23',
-            'modified' => '2017-11-10 09:27:23',
-            'enabled' => true,
-            'core_type' => true,
-            'translation_rules' => null,
-            'is_translatable' => true,
-        ];
-
-        parent::init();
-    }
+    // Empty fixture to inherit all data from parent class and be ready for customization.
 }
